429cf86af7aba2a531be3e5d256a9f4da43ff051
Romis <romis.nesmelov@gmail.com>
2023-10-18 19:28:30 +0300

    fixed some bug

--- resources/views/admin/repair_spare.blade.php
+++ resources/views/admin/repair_spare.blade.php
@@ -6,12 +6,11 @@
         @include('admin.blocks.hidden_id_block',['hiddenName' => 'repair_id', 'value' => request()->parent_id])
         <div class="panel panel-flat">
             @include('admin.blocks.title_block')
-            {{ request()->id }}
             <div class="panel-body">
                 @include('admin.blocks.select_block',[
                     'name' => 'spare_id',
                     'values' => $free_spares,
-                    'selected' => request()->has('id') ? request()->id : 1,
+                    'selected' => 1,
                     'option' => 'head'
                 ])
                 @include('admin.blocks.save_button_block')
