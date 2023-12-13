0c6921e8c5ccf0ac0b38e589df929048e6b6d485
Romis <romis@nesmelov.com>
2023-12-13 16:48:56 +0300

    fixed record table

--- resources/views/admin/blocks/records/table_record_cell_block.blade.php
+++ resources/views/admin/blocks/records/table_record_cell_block.blade.php
@@ -3,6 +3,6 @@
         <img class="brand" title="{{ $currentRecord->title.' '.$currentRecord->email.' '.($currentRecord->phone ? $currentRecord->phone : '').' '.($currentRecord->name ? $currentRecord->name : '') }}" src="{{ asset($record->carLink->brand->logo) }}" />
     </div>
 @endif
-{{--<img class="car" src="{{ $record->car_id ? $record->carLink->preview : asset('images/car_placeholder.jpg') }}">--}}
-<div class="record-title">{{ $record->car_id ? ucfirst($record->carLink->brand->eng).' '.$record->carLink->eng : ucfirst($record->car) }}</div>
+{{--<img class="car" src="{{ $record->car_id ? $record->carLink->image_preview : asset('images/car_placeholder.jpg') }}">--}}
+<div class="record-title">{{ $record->car_id ? ucfirst($record->carLink->brand->name_en).' '.$record->carLink->name_en : ucfirst($record->car) }}</div>
 <div>{{ $record->title }}</div>
